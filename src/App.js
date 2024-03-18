1fb4e7b30e3b54955e29d6bca2cc04f07a8d2800
mojtabank14 <mojtaba.ahwaz1414@gmail.com>
2024-03-18 08:35:42 -0700

    Modify sidebar style , add a new section.

--- src/App.js
+++ src/App.js
@@ -20,9 +20,19 @@ function App() {
               
           </section>
            {/* biographi */}
-          <aside className="bio bg-[#20202a] 
+          <aside className="bio bg-[#20202a] relative 
           ">
-              <header className='bio__header h-[235px] flex flex-col items-center justify-center'>
+          {/* bio's header */}
+          <footer className='bio__footer absolute 
+          right-0 left-0 
+          bottom-0 bg-red-100'>
+            slm
+          </footer>
+              <header className='bio__header
+               h-[235px] flex flex-col 
+               items-center absolute 
+               left-0 right-0  justify-center px-8'>
+
                 <div className='bio__header-img relative mb-2'>
                   <span className='absolute w-3.5 h-3.5
                    rounded-full bg-[#ffc107]
@@ -32,10 +42,10 @@ function App() {
                 </div>
 
                   <ul className='bio__header-detail'>
-                    <li className='mb-2 text-sm text-[#fafafa] '>
+                    <li className='mb-2 text-sm text-center text-[#fafafa] '>
                       محمد مجی
                     </li>
-                    <li className='mb-1 text-xs text-gray-400'>
+                    <li className='mb-1 text-xs text-center text-gray-400'>
                         جونیور دولوپر
                     </li>
                     <li className='text-sm text-gray-400'>
@@ -45,6 +55,50 @@ function App() {
 
 
               </header>
+              {/* bio content */}
+              <div className='pt-[235px] text-white px-8
+               h-[calc(100vh-40px)] overflow-x-auto
+                overflow-x-hidden flex flex-col'>
+                    <ul className='bio__personal-details justify-between items-center mb-2'>
+                        <li className='flex flex-row justify-between items-center mb-2'>
+                          <span>
+                          ملیت
+                          </span>
+                          <p>
+                            ایرانی
+                          </p>
+
+                        </li>
+                        <li className='flex flex-row justify-between items-center mb-2'>
+                          <span>
+                            شهر
+                          </span>
+                          <p>
+                            اهواز
+                          </p>
+
+                        </li>
+                        <li className='flex flex-row justify-between items-center mb-2'>
+                          <span>
+                            تلفن
+                          </span>
+                          <p>
+                            09023151876
+                          </p>
+
+                        </li>
+                        <li className='flex flex-row justify-between items-center mb-2'>
+                          <span>
+                            سن
+                          </span>
+                          <p>
+                            17
+                          </p>
+
+                        </li>
+                    </ul>
+                    <span className='bio-divider w-full h-[1px] bg-[#8c8c8e] opacity-40'/>
+              </div>
               
           </aside>
 
